12582d7ff562fa4e84c035f9b5c66a61f5cf1514
Terry Tilley <terry@terrytilley.com>
2020-06-04 13:27:21 +0100

    :tada: init commit :ring:

---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <v-row>
+          <v-col>
+            <nuxt-child />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null
+  })
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout column>
+    <v-flex xs12 sm8 md6>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+        laborum commodi eum illo repellat tempore quod blanditiis eveniet
+        ducimus perferendis praesentium, porro architecto mollitia consequuntur
+        dolorum iure et quam. Natus?
+      </p>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {}
+</script>
